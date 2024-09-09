79f69c0b6ede0f4317c4efa9a7222186d74c75a2
Tuul29 <tuul.tsegmed0329@gmail.com>
2024-09-09 12:57:53 +0800

    mm

---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,18 @@
+const getAllUser = (req, res) => {
+  res.json({ message: "Get Succese" });
+};
+
+const createUser = () => {};
+
+const updateUser = () => {};
+
+const deleteUser = () => {};
+const getCurrentUser = () => {};
+
+module.exports = {
+  getAllUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  getCurrentUser,
+};
